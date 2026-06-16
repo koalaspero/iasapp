5de1163499ebdc4f80037a757aa870fdd2ecd70f
koalaspero <diedarte@espol.edu.ec>
2026-06-16 00:37:44 -0500

    Cambios postgresql

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,22 +1,36 @@
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const adapter = new PrismaBetterSqlite3({
-  url: "file:./prisma/dev.db",
-});
-
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    adapter,
-  });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
 
+
+// import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// const adapter = new PrismaBetterSqlite3({
+//   url: "file:./prisma/dev.db",
+// });
+
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     adapter,
+//   });
+
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = prisma;
+// }
 
